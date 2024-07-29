c07ef3bea0372f650ba3e5df7bc2fbca627b1875
maryam <maryam.mhashemi1995@gmail.com>
2024-07-29 18:25:55 +0330

    add item for packingList for show

---
+++ src/components/item/index.jsx
@@ -0,0 +1,11 @@
+export default function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" value={item.packed} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
